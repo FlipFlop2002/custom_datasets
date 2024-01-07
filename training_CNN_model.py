855d7ae1a9da5ba0be3780237e7570563871b3b5
Filip Wronski <wronskifilip@wp.pl>
2024-01-08 00:23:14 +0100

    Created train_step and test_step functions; Created train function to train the model and print the results;

--- training_CNN_model.py
+++ training_CNN_model.py
@@ -8,6 +8,7 @@ from typing import Tuple, Dict, List
 import random
 import matplotlib.pyplot as plt
 from torch import nn
+from tqdm.auto import tqdm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -128,4 +129,121 @@ from torchinfo import summary
 summary(model_0, input_size=[32, 3, 64, 64])
 
 
-# creating functions to train and test the model
+
+### creating functions to train and test the model
+
+def train_step(model: torch.nn.Module,
+               train_dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device: torch.device):
+    model.train()
+    train_loss, train_acc = 0, 0
+
+    for batch, (X, y) in enumerate(train_dataloader):
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        y_pred_probs = torch.softmax(y_pred, dim=1)  # probabilities from 0 to 1
+        y_pred_class = y_pred_probs.argmax(dim=1)  # predicted label (0/1/2)
+        train_acc += (y_pred_class==y).sum().item()/len(y_pred)
+
+    train_loss = train_loss / len(train_dataloader)
+    train_acc = train_acc / len(train_dataloader)
+    return train_loss, train_acc
+
+
+def test_step(model: torch.nn.Module,
+               test_dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               device: torch.device):
+    model.eval()
+    test_loss, test_acc = 0, 0
+
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(test_dataloader):
+            X, y = X.to(device), y.to(device)
+
+            test_pred_logits = model(X)
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            test_pred_probs = torch.softmax(test_pred_logits, dim=1)  # probabilities from 0 to 1
+            test_pred_labels = test_pred_probs.argmax(dim=1)   # predicted label (0/1/2)
+            test_acc += (test_pred_labels==y).sum().item()/len(test_pred_labels)
+
+        test_loss = test_loss / len(test_dataloader)
+        test_acc = test_acc / len(test_dataloader)
+        return test_loss, test_acc
+
+
+
+### create a function to train and test the model 
+def train(model: torch.nn.Module,
+               train_dataloader: torch.utils.data.DataLoader,
+               test_dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device: torch.device,
+               num_epochs: int):
+    
+    results = {
+        "train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+    
+    for epoch in tqdm(range(num_epochs)):
+        train_loss, train_acc = train_step(model, train_dataloader, loss_fn, optimizer, device)
+        test_loss, test_acc = test_step(model, test_dataloader, loss_fn, device)
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+        print(f"Epoch: {epoch}\n")
+        print(f"Train loss: {train_loss:.4f} | Train acc: {train_acc:.4f} | Test loss: {test_loss:.4f} | Test acc: {test_acc:.4f}\n")
+        print("############################################################\n")
+    
+    return results
+
+
+
+### train the model
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+model_0 = TinyVGG(input_shape=3,
+                  hidden_units=10,
+                  output_shape=len(train_data_model_0_simple.classes)).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+EPOCHS = 5
+from timeit import default_timer as timer
+start_time = timer()
+
+
+model_0_results = train(model=model_0,
+      train_dataloader=train_dataloader_simple,
+      test_dataloader=test_dataloader_simple,
+      loss_fn=loss_fn,
+      optimizer=optimizer,
+      device=device,
+      num_epochs=EPOCHS)
+
+end_time = timer()
+print(f"Total training tim {end_time-start_time:.3f} seconds.")
+
+
+### plottig the loss curves of model_0
+
