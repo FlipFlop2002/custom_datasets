258f8b8f5d163d07fb5b7ce4de6fa151491c53d0
Filip Wronski <wronskifilip@wp.pl>
2024-01-08 19:11:03 +0100

    created a function to plot accuracy and loss curves

--- training_CNN_model.py
+++ training_CNN_model.py
@@ -246,4 +246,34 @@ print(f"Total training tim {end_time-start_time:.3f} seconds.")
 
 
 ### plottig the loss curves of model_0
+def plot_loss_curves(results: dict[str, list[float]]):
+    """Plots training ccurves of a results dictionary"""
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+
+    acc = results["train_acc"]
+    test_acc = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, acc, label="train_accuracy")
+    plt.plot(epochs, test_acc, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+    plt.show()
+
+
+plot_loss_curves(model_0_results)
+
 
